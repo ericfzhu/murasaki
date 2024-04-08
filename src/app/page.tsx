afc4dee990c4cc2e857d24cf5a2d64a7b474d7be
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-04-08 19:10:35 +1000

    mvoe font to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image';
-import { Architects_Daughter, Shadows_Into_Light_Two } from 'next/font/google';
-import { cn } from '@/lib/utils';
 
-const architectsDaughter = Architects_Daughter({ subsets: ['latin'], weight: ['400'] });
-const shadowsIntoLightTwo = Shadows_Into_Light_Two({ subsets: ['latin'], weight: ['400'] });
+import { cn } from '@/lib/utils';
 
 export default function Home() {
 	return (
-		<main className={cn('bg-main-600 flex min-h-screen flex-col items-center justify-center p-24', architectsDaughter.className)}>
+		<main className={cn('flex min-h-screen flex-col items-center justify-center bg-main-600 p-24')}>
 			<Image className="-mb-16" src="/hat.png" alt="" height="500" width="500" />
-			<div className="text-accent-500 text-6xl">Murasaki</div>
+			<div className="text-6xl text-accent-500">Murasaki</div>
 		</main>
 	);
 }
